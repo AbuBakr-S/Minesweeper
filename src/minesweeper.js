1ab244edf5f78409a74d4161d1de3c6152be49e6
AbuBakr-S <abubakr@abubakars-air.home>
2018-12-25 19:00:13 +0000

    Complete first part of Minesweeper project

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+// Empty game board
+const blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Hard-coding in an active game board
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log('This is what a board with a guess and a bomb on it would look like:');
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
